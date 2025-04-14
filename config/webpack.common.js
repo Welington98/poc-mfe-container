8d79ab596de5a64d4f8cbca31eb76523214953dd
welington98 <welingtonoliveira98@gmail.com>
2025-04-14 09:03:27 -0300

    Add .gitignore, configure CopyWebpackPlugin, and create _redirects file

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   module: {
@@ -19,6 +20,11 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'public/_redirects', to: './' }
+      ],
     }),
   ],
 };
